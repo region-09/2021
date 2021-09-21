5198af58bd421d3b5191d06e6c66328c26a7aac0
Almaz-119 <90274022+Almaz-119@users.noreply.github.com>
2021-09-21 22:44:53 +0200

    VISIBLE rectangle and black background.
    Public Main() {  .......  } added!!!! Most importantly

--- p01.java
+++ p01.java
@@ -1,28 +1,34 @@
 import java.awt.*;
+import java.util.*;
 import javax.swing.*;
 
 
-public class Main extends MyPanel{
+public class Main extends JPanel{
+    Scanner sc = new Scanner(System.in);
+    static int x = 110;
+    static int y = 110;
 
     public static void main(String[] args) {
-        MyPanel panel = new MyPanel();
         JFrame jf = new JFrame("JFrame example");
         jf.setSize(300, 200);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.add(new Main());
+        jf.setVisible(true);
+    }
+    public Main() {
+        setBackground(Color.black);
         JButton b = new JButton("Click");
         b.setBounds(10, 10, 60, 20);
         b.setVisible(true);
-        jf.setVisible(true);
-        jf.add(b);
-        jf.add(panel);
     }
-}
-class MyPanel extends JPanel {
-    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        g.setColor(Color.BLACK);
-        g.fillRect(30,30,30,30);
+        g.setColor(Color.red);
+        g.fillRect(x,y,30,30);
     }
 }
+
+/*JFrame jf = new JFrame("JFrame example");
+        jf.setSize(300, 200);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);*/
